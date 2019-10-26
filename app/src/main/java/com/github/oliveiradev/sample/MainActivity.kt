62f6aa17f6b87a30b662f70043b6400297eee518
Felipe Oliveira <felipe.oliveiradev@gmail.com>
2019-10-26 15:31:52 -0300

    get bitmap as capture image result

--- app/src/main/java/com/github/oliveiradev/sample/MainActivity.kt
+++ app/src/main/java/com/github/oliveiradev/sample/MainActivity.kt
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 import com.github.oliveiradev.kapture.takePhoto
 
 class MainActivity : AppCompatActivity() {
 
+    private val imageResult by lazy { findViewById<ImageView>(R.id.bitmap) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        takePhoto {
-            Log.d("RESULT", "$it")
+        takePhoto { result ->
+            result.collectAsBitmap { bitmap, throwable ->
+                bitmap?.let(imageResult::setImageBitmap)
+                throwable?.let { error ->
+                    Toast.makeText(
+                        this,
+                        "Something was wrong ${error.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
         }
     }
 }
