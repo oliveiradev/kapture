44d5e72705e8f48a265530c0264a6be2ecd07e8d
Felipe Oliveira <felipe.oliveiradev@gmail.com>
2019-11-02 21:41:04 -0300

    Save image using work manager

--- kapture/src/main/java/com/github/oliveiradev/kapture/FileHelper.kt
+++ kapture/src/main/java/com/github/oliveiradev/kapture/FileHelper.kt
@@ -1,17 +1,35 @@
 package com.github.oliveiradev.kapture
 
+import android.graphics.Bitmap
+import android.net.Uri
+import com.github.oliveiradev.kapture.Constants.KAPTURE_FILE_CHILD
 import java.io.File
-import java.io.IOException
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.UUID
 
 object FileHelper {
 
-    @Throws(IOException::class)
-    fun createImageFile(storageDir: File): File {
+    fun writeBitmapToFile(dir: File?, bitmap: Bitmap?): Uri {
+        val outputFile = createDir(dir)
+        val out = FileOutputStream(outputFile)
+        out.use {
+            bitmap?.compress(Bitmap.CompressFormat.PNG, 0, it)
+        }
+        return Uri.fromFile(outputFile)
+    }
+
+    private fun createDir(dir: File?): File {
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
             .format(Date())
-        return File.createTempFile("JPEG_${timeStamp}_",".jpg", storageDir)
+        val name = String.format("%s-%s.png", UUID.randomUUID().toString(), timeStamp)
+        val outputDir = File(dir, KAPTURE_FILE_CHILD)
+        if (!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+
+        return File(outputDir, name)
     }
 }
--- kapture/src/main/java/com/github/oliveiradev/kapture/ResultProcessorFragment.kt
+++ kapture/src/main/java/com/github/oliveiradev/kapture/ResultProcessorFragment.kt
@@ -1,14 +1,26 @@
 package com.github.oliveiradev.kapture
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import androidx.fragment.app.Fragment
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.github.oliveiradev.kapture.Constants.KAPTURE_FRAGMENT_TAG
+import com.github.oliveiradev.kapture.worker.SaveFileWorker
+import java.util.UUID
+
+typealias WorkerId = UUID
 
 class ResultProcessorFragment : Fragment() {
 
-    private var kaptureResultListener: KaptureResultListener? = null
     private var intentExtra: Intent? = null
+    private var workManager: WorkManager? = null
+    private var onWorkerIdListener: OnWorkerIdListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,19 +37,22 @@ class ResultProcessorFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == KAPTURE_IMAGE_REQUEST) {
-            data?.putExtra(
-                KAPTURE_EXTERNAL_STORAGE,
-                context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            )
-
-            kaptureResultListener?.invoke(KaptureResult(data))
-            removeFragment()
+        if (requestCode == KAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            val dir = context?.applicationContext?.filesDir
+            val bitmap = data?.extras?.get("data") as? Bitmap
+            val uri = FileHelper.writeBitmapToFile(dir, bitmap)
+            val workerId = startWorkers(uri)
+            onWorkerIdListener?.invoke(workerId)
         }
+        removeFragment()
     }
 
-    fun setKaptureResultListener(kaptureResultListener: KaptureResultListener)  {
-        this.kaptureResultListener = kaptureResultListener
+    fun setWorkManager(workManager: WorkManager) {
+        this.workManager = workManager
+    }
+
+    fun setOnWorkerIdListener(onWorkerIdListener: OnWorkerIdListener) {
+        this.onWorkerIdListener = onWorkerIdListener
     }
 
     private fun processIntent(intent: Intent) {
@@ -52,11 +67,29 @@ class ResultProcessorFragment : Fragment() {
             ?.commitAllowingStateLoss()
     }
 
+    private fun startWorkers(uri: Uri): WorkerId {
+        val saveFileWorker = OneTimeWorkRequestBuilder<SaveFileWorker>()
+            .addTag(KAPTURE_FRAGMENT_TAG)
+            .setInputData(
+                Data.Builder()
+                    .putString(Constants.EXTRA_WORKER_URI, uri.toString())
+                    .build()
+            )
+            .build()
+
+        workManager?.enqueueUniqueWork(
+            KAPTURE_FRAGMENT_TAG,
+            ExistingWorkPolicy.REPLACE,
+            saveFileWorker
+        )
+
+        return saveFileWorker.id
+    }
+
     companion object {
 
         private const val KAPTURE_IMAGE_REQUEST = 1
         private const val KAPTURE_INTENT_EXTRA = "KAPTURE_INTENT_EXTRA"
-        private const val KAPTURE_EXTERNAL_STORAGE = "storage"
 
         fun newInstance(intent: Intent): ResultProcessorFragment {
             return ResultProcessorFragment().apply {
